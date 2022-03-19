5794248d7c5d87aad1153f6c757ba8df8d376d8f
cristcoder <44945683+cristcoder@users.noreply.github.com>
2022-03-19 11:40:16 -0500

    Test isPasswordEquals

--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -2,10 +2,17 @@ package com.tdea.pretest;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 class PretestApplicationTests {
 
     @Test
-    void contextLoads() {
+    void isPasswordEquals() {
+        String a = "clave";
+        String b = "clave";
+        UserUtils userUtils = new UserUtils();
+        assertTrue(userUtils.isPasswordEqual(a,b));
     }
 
 }
